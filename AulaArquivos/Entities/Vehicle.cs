c90b4e6a9b60339cb551d369d93adbd3ae342f33
allan.firme <justoallan129@gmail.com>
2023-08-14 20:57:19 -0300

    first commit

---
+++ AulaArquivos/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace AulaArquivos.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ AulaArquivos/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace AulaArquivos.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
